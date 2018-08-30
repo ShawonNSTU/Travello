61d8da6c02fda77a7b3270e57a3bfaa8bee02c4c
ShawonNSTU <shuvroshawon9@gmail.com>
2018-08-31 00:18:37 +0600

    Final Year Project

---
+++ app/src/main/java/com/example/shawon/travelbd/ModelClass/PlaceInfo.java
@@ -0,0 +1,115 @@
+package com.example.shawon.travelbd.ModelClass;
+
+import android.net.Uri;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by SHAWON on 8/29/2018.
+ */
+
+public class PlaceInfo {
+
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private String id;
+    private Uri websiteUri;
+    private LatLng latlng;
+    private float rating;
+    private String place_types;
+
+    public PlaceInfo(String name, String address, String phoneNumber, String id, Uri websiteUri,
+                     LatLng latlng, float rating, String place_types) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.id = id;
+        this.websiteUri = websiteUri;
+        this.latlng = latlng;
+        this.rating = rating;
+        this.place_types = place_types;
+    }
+
+    public PlaceInfo() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Uri getWebsiteUri() {
+        return websiteUri;
+    }
+
+    public void setWebsiteUri(Uri websiteUri) {
+        this.websiteUri = websiteUri;
+    }
+
+    public LatLng getLatlng() {
+        return latlng;
+    }
+
+    public void setLatlng(LatLng latlng) {
+        this.latlng = latlng;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public String getPlace_types() {
+        return place_types;
+    }
+
+    public void setPlace_types(String place_types) {
+        this.place_types = place_types;
+    }
+
+    @Override
+    public String toString() {
+        return "PlaceInfo{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", id='" + id + '\'' +
+                ", websiteUri=" + websiteUri +
+                ", latlng=" + latlng +
+                ", rating=" + rating +
+                ", place_types='" + place_types + '\'' +
+                '}';
+    }
+}
