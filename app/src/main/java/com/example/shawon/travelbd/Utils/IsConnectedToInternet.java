f9b4bde90a87f54552a9a2dff16c5ce573e59948
ShawonNSTU <shuvroshawon9@gmail.com>
2018-08-31 00:21:15 +0600

    Final Year Project

---
+++ app/src/main/java/com/example/shawon/travelbd/Utils/IsConnectedToInternet.java
@@ -0,0 +1,28 @@
+package com.example.shawon.travelbd.Utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Created by SHAWON on 8/30/2018.
+ */
+
+public class IsConnectedToInternet {
+
+    public static boolean isConnectedToInternet(Context context){
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connectivityManager != null){
+            NetworkInfo[] info = connectivityManager.getAllNetworkInfo();
+            if(info != null){
+                for(int i=0; i<info.length;i++){
+                    if(info[i].getState() == NetworkInfo.State.CONNECTED){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+}
