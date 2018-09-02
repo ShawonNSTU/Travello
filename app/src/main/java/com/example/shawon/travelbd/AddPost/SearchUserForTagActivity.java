cb7ac5bb335fb6a3b5bacf17f0abb1eafcdc5eac
ShawonNSTU <shuvroshawon9@gmail.com>
2018-09-02 23:23:19 +0600

    Final Year Project

---
+++ app/src/main/java/com/example/shawon/travelbd/AddPost/SearchUserForTagActivity.java
@@ -0,0 +1,27 @@
+package com.example.shawon.travelbd.AddPost;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.example.shawon.travelbd.R;
+
+/**
+ * Created by SHAWON on 9/2/2018.
+ */
+
+public class SearchUserForTagActivity extends AppCompatActivity {
+
+    private static final String TAG = "SearchUserForTag";
+    private Context context = SearchUserForTagActivity.this;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_user_for_tag);
+
+        Log.d(TAG, "onCreate : Started.");
+    }
+}
