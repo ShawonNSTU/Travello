919ac1154aadb055076bb7356e23ca75e824459d
ShawonNSTU <shuvroshawon9@gmail.com>
2018-09-03 10:52:30 +0600

    Final Year Project

--- app/src/main/java/com/example/shawon/travelbd/AddPost/SearchUserForTagActivity.java
+++ app/src/main/java/com/example/shawon/travelbd/AddPost/SearchUserForTagActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
 import com.example.shawon.travelbd.R;
@@ -21,6 +22,19 @@ public class SearchUserForTagActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_user_for_tag);
+
         Log.d(TAG, "onCreate : Started.");
+
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+
+        Log.d(TAG,"setupToolbar : Setting up toolbar in SearchUserForTag Activity");
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.searchUserForTagToolBar);
+
+        setSupportActionBar(toolbar);
+
     }
 }
