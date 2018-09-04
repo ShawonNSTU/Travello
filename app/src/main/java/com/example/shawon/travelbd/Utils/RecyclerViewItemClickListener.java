6557e696cc21ea97eeac5a49c995d6806d8f303a
ShawonNSTU <shuvroshawon9@gmail.com>
2018-09-04 22:15:53 +0600

    Final Year Project

---
+++ app/src/main/java/com/example/shawon/travelbd/Utils/RecyclerViewItemClickListener.java
@@ -0,0 +1,13 @@
+package com.example.shawon.travelbd.Utils;
+
+import android.view.View;
+
+/**
+ * Created by SHAWON on 9/4/2018.
+ */
+
+public interface RecyclerViewItemClickListener {
+
+    void onClick(View view, int position, boolean isLongClick );
+
+}
