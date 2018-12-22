7ffb5664fbd7928c5daffae940eb8db277e1be98
ShawonNSTU <shuvroshawon9@gmail.com>
2018-12-22 21:18:45 +0600

    Final Year Project

---
+++ app/src/main/java/com/example/shawon/travelbd/Utils/StringManipulation.java
@@ -0,0 +1,29 @@
+package com.example.shawon.travelbd.Utils;
+
+/**
+ * Created by SHAWON on 12/22/2018.
+ */
+
+public class StringManipulation {
+
+    public static String getTags(String caption) {
+        if (caption.indexOf('#') >= 0) {
+            StringBuilder stringBuilder = new StringBuilder();
+            char[] charArray = caption.toCharArray();
+            boolean found = false;
+            for (char ch : charArray){
+                if (ch == ' ') found = false;
+                else if (ch == '#'){
+                    found = true;
+                    stringBuilder.append(ch);
+                }
+                else if(found) stringBuilder.append(ch);
+            }
+            String s = stringBuilder.toString().replace("##", "#").replace("#", ",#");
+            return s.substring(1,s.length());
+        }
+        else{
+            return "No Tags";
+        }
+    }
+}
