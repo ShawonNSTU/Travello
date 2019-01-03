61878994d2951711318910808f80ff89e1844324
ShawonNSTU <shuvroshawon9@gmail.com>
2019-01-04 01:22:20 +0600

    Final Year Project

--- app/src/main/java/com/example/shawon/travelbd/SearchDestinationPlaces/SearchDestinationPlacesActivity.java
+++ app/src/main/java/com/example/shawon/travelbd/SearchDestinationPlaces/SearchDestinationPlacesActivity.java
@@ -35,27 +35,33 @@ public class SearchDestinationPlacesActivity extends AppCompatActivity implement
 
     private static final LatLngBounds LAT_LNG_BOUNDS = new LatLngBounds(new LatLng(-40,-168),new LatLng(71,136));
     private GoogleApiClient mGoogleApiClientForGooglePlaces;
-    private PlaceAutocompleteAdapter mPlaceAutocompleteAdapter = null;
+    private PlaceAutocompleteAdapter mPlaceAutocompleteAdapter;
+    private boolean isGoogleApiClientConnected;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("");
         setContentView(R.layout.activity_search_destination_places);
-        Log.d(TAG,"onCreate: Started.");
+        Log.d(TAG,"onCreate : Started.");
 
         Toolbar mToolbar = (Toolbar) findViewById(R.id.search_toolbar);
         setSupportActionBar(mToolbar);
 
         if (IsConnectedToInternet.isConnectedToInternet(context)){
+            Log.d(TAG,"IsConnectedToInternet : Success");
             buildGoogleApiClient();
+            isGoogleApiClientConnected = true;
         }
         else {
+            Log.d(TAG,"IsConnectedToInternet : Failed");
+            isGoogleApiClientConnected = false;
             Toast.makeText(context,"Please check your internet connection.",Toast.LENGTH_SHORT).show();
         }
     }
 
     private void buildGoogleApiClient() {
+        Log.d(TAG,"build GoogleApiClient For Google Places : started");
 
         mGoogleApiClientForGooglePlaces = new GoogleApiClient
                 .Builder(this)
@@ -64,6 +70,7 @@ public class SearchDestinationPlacesActivity extends AppCompatActivity implement
                 .enableAutoManage(this, this)
                 .build();
 
+        // Initialization of Google Places Adapter...
         mPlaceAutocompleteAdapter = new PlaceAutocompleteAdapter(context,mGoogleApiClientForGooglePlaces,LAT_LNG_BOUNDS,null);
     }
 
@@ -73,23 +80,27 @@ public class SearchDestinationPlacesActivity extends AppCompatActivity implement
 
         getMenuInflater().inflate(R.menu.search_destination_places_toolbar_menu,menu);
 
-        //getting the search view of the menu...
+        // getting the search view of the menu...
         SearchView searchView = (SearchView) menu.findItem(R.id.search_destination_places).getActionView();
         searchView.setIconified(false);
         searchView.setQueryHint(Html.fromHtml("<small>"+getString(R.string.where_do_you_want_to_go)+"</small>"));
 
-        //getting edit text of the search view to change it's text properties...
+        // getting edit text of the search view to change it's text properties...
         EditText textSearch = ((EditText)searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text));
         textSearch.setTextColor(getResources().getColor(R.color.light_black));
         textSearch.setTextSize(18);
-        SearchView.SearchAutoComplete searchAutoComplete = (SearchView.SearchAutoComplete)searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
-        if (mPlaceAutocompleteAdapter != null) searchAutoComplete.setAdapter(mPlaceAutocompleteAdapter);
+
+        // getting the AutoCompleteSuggestion feature of the search view to add the adapter...
+        SearchView.SearchAutoComplete searchAutoComplete = (SearchView.SearchAutoComplete)searchView
+                .findViewById(android.support.v7.appcompat.R.id.search_src_text);
+        if (isGoogleApiClientConnected) searchAutoComplete.setAdapter(mPlaceAutocompleteAdapter);
+
         return true;
     }
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
-
+        // later code
     }
 
     @Override
@@ -99,12 +110,13 @@ public class SearchDestinationPlacesActivity extends AppCompatActivity implement
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
+        // later code
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+
         if (mGoogleApiClientForGooglePlaces != null){
             mGoogleApiClientForGooglePlaces.disconnect();
         }
