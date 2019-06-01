62a30a01f522adfc87798cc175448d04e7cec741
ShawonNSTU <shuvroshawon9@gmail.com>
2019-06-02 00:44:48 +0600

    Final Year Project

--- app/src/main/java/com/example/shawon/travelbd/Search/SearchActivity.java
+++ app/src/main/java/com/example/shawon/travelbd/Search/SearchActivity.java
@@ -4,13 +4,31 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import com.example.shawon.travelbd.ModelClass.UserPersonalInfo;
 import com.example.shawon.travelbd.R;
 import com.example.shawon.travelbd.Utils.BottomNavigationViewHelper;
+import com.example.shawon.travelbd.Utils.UserListAdapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by SHAWON on 7/17/2018.
@@ -22,33 +40,115 @@ public class SearchActivity extends AppCompatActivity {
     private static final int ACTIVITY_NUM = 1;
     private Context context = SearchActivity.this;
 
+    //widgets
+    private EditText mSearchParam;
+    private ListView mListView;
+
+    //vars
+    private List<UserPersonalInfo> mUserList;
+    private UserListAdapter mAdapter;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_search);
+        mSearchParam = (EditText) findViewById(R.id.search);
+        mListView = (ListView) findViewById(R.id.listView);
         Log.d(TAG, "SearchActivity onCreate: Starting.");
 
+        hideSoftKeyboard();
         setupBottomNavigationView();
+        initTextListener();
+    }
 
+    private void initTextListener(){
+        Log.d(TAG, "initTextListener: initializing");
+
+        mUserList = new ArrayList<>();
+
+        mSearchParam.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String text = mSearchParam.getText().toString();
+                searchForMatch(text);
+            }
+        });
+    }
+
+    private void searchForMatch(String keyword){
+        Log.d(TAG, "searchForMatch: searching for a match: " + keyword);
+        mUserList.clear();
+        //update the users list view
+        if(keyword.length() == 0){
+            // later code...
+        }
+        else{
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+            Query query = reference.child(getString(R.string.user_personal_Info))
+                    .orderByChild(getString(R.string.field_username)).startAt(keyword).endAt(keyword + "\uf8ff");
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for(DataSnapshot singleSnapshot :  dataSnapshot.getChildren()){
+                        Log.d(TAG, "onDataChange: found user:" + singleSnapshot.getValue(UserPersonalInfo.class).toString());
+
+                        mUserList.add(singleSnapshot.getValue(UserPersonalInfo.class));
+                        //update the users list view
+                        updateUsersList();
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+    }
+
+    private void updateUsersList() {
+        Log.d(TAG, "updateUsersList: updating users list");
+
+        mAdapter = new UserListAdapter(SearchActivity.this, R.layout.layout_user_listitem, mUserList);
+
+        mListView.setAdapter(mAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemClick: selected user: " + mUserList.get(position).toString());
+
+                //navigate to profile activity
+
+            }
+        });
+    }
+
+    private void hideSoftKeyboard(){
+        if(getCurrentFocus() != null){
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
     }
 
     // BottomNavigationView Setup
-
     private void setupBottomNavigationView() {
-
         Log.d(TAG, "SearchActivity setupBottomNavigationView: setting up BottomNavigationView");
-
         BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
-
         BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
-
         BottomNavigationViewHelper.enableNavigation(context, bottomNavigationViewEx);
-
         Menu menu = bottomNavigationViewEx.getMenu();
-
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
-
         menuItem.setChecked(true);
-
     }
 }
