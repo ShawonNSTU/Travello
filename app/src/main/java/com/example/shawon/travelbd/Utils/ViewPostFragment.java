153035ffff963f29fd04bf0b55ddc5e11fdf1e55
ShawonNSTU <shuvroshawon9@gmail.com>
2019-06-03 14:31:17 +0600

    Final Year Project

--- app/src/main/java/com/example/shawon/travelbd/Utils/ViewPostFragment.java
+++ app/src/main/java/com/example/shawon/travelbd/Utils/ViewPostFragment.java
@@ -400,7 +400,7 @@ public class ViewPostFragment extends Fragment {
                 .setValue(like);
 
         myRef.child(getString(R.string.user_photos))
-                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .child(mPhoto.getUser_id())
                 .child(getString(R.string.uploaded))
                 .child(mPhoto.getPhoto_id())
                 .child(getString(R.string.field_likes))
