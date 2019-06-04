fe03f57124d9e3b1bd8339d37f4cfcbf9829b54a
ShawonNSTU <shuvroshawon9@gmail.com>
2019-06-04 15:04:22 +0600

    Final Year Project

--- app/src/main/java/com/example/shawon/travelbd/Utils/MainfeedListAdapter.java
+++ app/src/main/java/com/example/shawon/travelbd/Utils/MainfeedListAdapter.java
@@ -124,13 +124,14 @@ public class MainfeedListAdapter extends ArrayAdapter<Photo> {
         //set the comment
         List<Comment> comments = getItem(position).getComments();
         holder.comments.setText("View all " + comments.size() + " comments");
-        holder.comment.setOnClickListener(new View.OnClickListener() {
+        holder.comments.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d(TAG, "onClick: loading comment thread for " + getItem(position).getPhoto_id());
-                ((HomeActivity)mContext).onCommentThreadSelected(getItem(position), holder.settings);
+                ((HomeActivity)mContext).onCommentThreadSelected(getItem(position),mContext.getString(R.string.home_activity));
 
                 //going to need to do something else?
+                ((HomeActivity)mContext).hideLayout();
             }
         });
 
@@ -194,9 +195,10 @@ public class MainfeedListAdapter extends ArrayAdapter<Photo> {
                     holder.comment.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            ((HomeActivity)mContext).onCommentThreadSelected(getItem(position), holder.settings);
+                            ((HomeActivity)mContext).onCommentThreadSelected(getItem(position),mContext.getString(R.string.home_activity));
 
                             //another thing?
+                            ((HomeActivity)mContext).hideLayout();
                         }
                     });
                 }
@@ -390,7 +392,7 @@ public class MainfeedListAdapter extends ArrayAdapter<Photo> {
 
                                 String[] splitUsers = holder.users.toString().split(",");
 
-                                if(holder.users.toString().contains(holder.user.getUsername() + ",")){
+                                if(holder.users.toString().contains(currentUsername  + ",")){
                                     holder.likeByCurrentUser = true;
                                 }else{
                                     holder.likeByCurrentUser = false;
