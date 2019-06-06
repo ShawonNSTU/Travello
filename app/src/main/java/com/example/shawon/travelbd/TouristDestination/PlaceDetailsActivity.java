5fca84a6d8c11126a9c4f54e651635845c15578a
ShawonNSTU <shuvroshawon9@gmail.com>
2019-06-07 02:13:05 +0600

    Final Year Project

---
+++ app/src/main/java/com/example/shawon/travelbd/TouristDestination/PlaceDetailsActivity.java
@@ -0,0 +1,20 @@
+package com.example.shawon.travelbd.TouristDestination;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.shawon.travelbd.R;
+
+/**
+ * Created by SHAWON on 6/6/2019.
+ */
+
+public class PlaceDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_details);
+    }
+}
