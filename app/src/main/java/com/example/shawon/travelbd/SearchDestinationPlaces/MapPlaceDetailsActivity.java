3da932b09e9a0779f26ac4a1fcdddab63e9caf1f
ShawonNSTU <shuvroshawon9@gmail.com>
2019-06-09 01:16:01 +0600

    Final Year Project

---
+++ app/src/main/java/com/example/shawon/travelbd/SearchDestinationPlaces/MapPlaceDetailsActivity.java
@@ -0,0 +1,22 @@
+package com.example.shawon.travelbd.SearchDestinationPlaces;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.shawon.travelbd.R;
+
+/**
+ * Created by SHAWON on 6/9/2019.
+ */
+
+public class MapPlaceDetailsActivity extends AppCompatActivity {
+
+    private static final String TAG = " MapPlaceDetails";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_place_details);
+    }
+}
