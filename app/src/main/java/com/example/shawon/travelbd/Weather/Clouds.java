3ebd90cfcb2a66b6f1d125b007973e23afd0d96e
ShawonNSTU <shuvroshawon9@gmail.com>
2019-06-12 00:13:28 +0600

    Final Year Project

---
+++ app/src/main/java/com/example/shawon/travelbd/Weather/Clouds.java
@@ -0,0 +1,22 @@
+package com.example.shawon.travelbd.Weather;
+
+/**
+ * Created by SHAWON on 6/11/2019.
+ */
+
+public class Clouds {
+
+    private int all;
+
+    public Clouds(int all) {
+        this.all = all;
+    }
+
+    public void setAll(int all){
+        this.all = all;
+    }
+    public int getAll(){
+        return all;
+    }
+
+}
