ee39d670eb3787d76e6b9584e11462790675785b
ShawonNSTU <shuvroshawon9@gmail.com>
2019-06-21 15:34:12 +0600

    Final Year Project

---
+++ app/src/main/java/com/example/shawon/travelbd/FavouritePlace/FavouritePlaceItemViewHolder.java
@@ -0,0 +1,42 @@
+package com.example.shawon.travelbd.FavouritePlace;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.shawon.travelbd.R;
+import com.example.shawon.travelbd.Utils.RecyclerViewItemClickListener;
+
+/**
+ * Created by SHAWON on 6/21/2019.
+ */
+
+public class FavouritePlaceItemViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    public TextView placeName;
+    public ImageView placeImage;
+    public ImageView favorite;
+
+    private RecyclerViewItemClickListener itemClickListener;
+
+    public FavouritePlaceItemViewHolder(View itemView) {
+        super(itemView);
+
+        placeName = (TextView) itemView.findViewById(R.id.favourite_place_name);
+        placeImage = (ImageView) itemView.findViewById(R.id.favourite_place_image);
+        favorite = (ImageView) itemView.findViewById(R.id.favourite);
+
+        itemView.setOnClickListener(this);
+
+    }
+
+    public void setItemClickListener(RecyclerViewItemClickListener itemClickListener) {
+        this.itemClickListener = itemClickListener;
+    }
+
+    @Override
+    public void onClick(View v) {
+        itemClickListener.onClick(v,getAdapterPosition(),false);
+    }
+}
